199687cd2bee33f54beb2ebecd931db5c21f8fcd
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2025-03-10 17:45:22 +0600

    refactor main function to use bufio for user input and improve input handling

--- main.go
+++ main.go
@@ -1,21 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
 	var id int
 	var amount float32
 	var name string
 
-	fmt.Print("Enter the id:")
-	fmt.Scan(&id)
-	fmt.Print("Enter the name:")
-	fmt.Scan(&name)
-	fmt.Print("Enter the amount:")
-	fmt.Scan(&amount)
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("Enter the id: ")
+	scanner.Scan()
+	id, _ = strconv.Atoi(scanner.Text())
+
+	fmt.Print("Enter the name: ")
+	scanner.Scan()
+	name = scanner.Text()
+
+	fmt.Print("Enter the amount: ")
+	scanner.Scan()
+	amountFloat, _ := strconv.ParseFloat(scanner.Text(), 32)
+	amount = float32(amountFloat)
 
 	fmt.Println("Id:", id)
 	fmt.Println("Name:", name)
 	fmt.Println("Amount:", amount)
-
 }
