d4702d22da8f05e20ad3fd5c47f549986008d570
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2025-03-10 17:41:02 +0600

    update main function to accept user input for id, name, and amount

--- main.go
+++ main.go
@@ -3,6 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println("Yoo buddy")
+	var id int
+	var amount float32
+	var name string
+
+	fmt.Print("Enter the id:")
+	fmt.Scan(&id)
+	fmt.Print("Enter the name:")
+	fmt.Scan(&name)
+	fmt.Print("Enter the amount:")
+	fmt.Scan(&amount)
+
+	fmt.Println("Id:", id)
+	fmt.Println("Name:", name)
+	fmt.Println("Amount:", amount)
+
 }
